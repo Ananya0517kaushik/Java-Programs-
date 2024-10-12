655ad214b408dae629413f0ec77860fdb3d6e948
ANANYA KAUSHIK <147698162+Ananya0517kaushik@users.noreply.github.com>
2024-10-12 21:30:57 +0530

    Add files via upload
    Normal pyramid using java

---
+++ normal pyramid.java
@@ -0,0 +1,15 @@
+public class noraml pyramid{
+	public static void main(String args[]){
+
+		int rows=5;
+		for(int i=1;i<=rows;i++){
+			for(j=1;j<rows;j++){
+				System.out.print("*");
+			}
+			for (k=1;k<=i;k++){
+				System.out.print("*");
+				System.out,println();				
+			}
+			}
+			}
+		}
